33deb8fa54906ab3afce3305d14428d8e093aa23
Arthur Kim <arthur.kim@g.ucla.edu>
2022-02-18 22:55:36 -0800

    Finished HW6. feelsgoodman.

--- hw6/Assignment06.hs
+++ hw6/Assignment06.hs
@@ -36,9 +36,24 @@ f :: (Ord nt, Ord t, Semiring a) => GenericCFG nt t a -> [t] -> a
 f m w = let (nt, t, i, r) = m in
   gen_or (map (\n -> i n &&& fastInside m w n) nt)
 
+-- In case anyone sees this pointless comment, I've finally come to the point
+-- where I genuinely, *genuinely* have no clue what the hell any of my code
+-- is doing anymore. I'm just basing this implementation on how inside is
+-- implemented, substituting variables whenever needed
 outside :: (Ord nt, Ord t, Semiring v) => GenericCFG nt t v -> ([t],[t]) -> nt -> v
 outside cfg str n =
   let (nt, t, i, r) = cfg in
     case str of
       ([], []) -> i n
-      (x : [], y : []) -> 
+      -- What kind of magic nonsense is going on here? Seems to work but????
+      (x, y) -> let conj1 i1 p1 rd = outside cfg (x, drop i1 y) p1 &&&
+                                     r (NTRule p1 (n, rd)) &&&
+                                     inside cfg y rd
+                in
+                  let conj2 i2 p2 ld = outside cfg (take i2 x, y) p2 &&&
+                                       r (NTRule p2 (ld, n)) &&&
+                                       inside cfg (drop i2 x) ld
+                  in
+                    (gen_or (liftA3 conj1 [1 .. (length y)] nt nt)) |||
+                    (gen_or (liftA3 conj2 [0 .. (length x - 1)] nt nt))
+                  
