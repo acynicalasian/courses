9d20f461a581718d6e2a05baa249f64bcd11a5c5
Arthur Kim <arthur.kim@g.ucla.edu>
2022-03-05 20:39:39 -0800

    Finished HW8.

--- hw8/Assignment08.hs
+++ hw8/Assignment08.hs
@@ -81,16 +81,77 @@ leftEdge :: Tree a -> [a]
 leftEdge t = case t of
   Node x y -> x : (case y of { [] -> []; z : rest -> leftEdge z })
 
-addChar :: [a] -> [a] -> [[a]]
-addChar str alpha = case alpha of
+addChar :: [[a]] -> [a] -> [[a]]
+addChar a b = case b of
   [] -> []
-  a : rest -> (str ++ [a]) : addChar str rest
+  x : rest -> map (\a -> a ++ [x]) a ++ addChar a rest
 
 allLists :: Int -> [a] -> [[a]]
 allLists i s = case (i > 1) of
-  True -> map (\n -> addChar n s) (allLists (i - 1) s)
-  False -> case i of
-    1 -> addChar s s
-    0 -> []
+  True -> addChar (allLists (i - 1) s) s
+  False -> case (i > 0) of
+    True -> map (\s -> [s]) s
+    False -> []
 
+under :: (Eq st, Eq sy) => Automaton st sy -> Tree sy -> st -> Bool
+under auto tree q =
+  let (states, alpha, fin, trans) = auto in
+    case tree of
+      Node x [] -> elem ([], x, q) trans
+      Node x y -> let conj qs = elem (qs, x, q) trans &&
+                                  and (map (\(t, q) -> under auto t q) (zip y qs))
+                  in
+                    or (liftA conj (allLists (length y) states))
 
+generates :: (Eq st, Eq sy) => Automaton st sy -> Tree sy -> Bool
+generates auto t =
+  let (states, alpha, fin, trans) = auto in
+    or (map (\q -> under auto t q && elem q fin) states)
+
+data State = Null | Wh | Q deriving (Show, Eq)
+fsta_wh1 :: Automaton State String
+fsta_wh1 =
+  let
+    states = [Null, Wh, Q]
+    syms = plainWords ++ whWords ++ qWords ++ ["*"]
+    f = [Null]
+    trans =
+      -- plainWords leaf nodes
+      map (\w -> ([], w, Null)) plainWords ++
+      -- whWords leaf nodes
+      map (\w -> ([], w, Wh)) whWords ++
+      -- qWords leaf nodes
+      map (\w -> ([], w, Q)) qWords ++
+      -- non-terminal nodes
+      -- Q-states not being allowed to have children guarantees Q will c-command wh-words
+      [ ([Null, Null], "*", Null), ([Wh, Null], "*", Wh),
+        ([Null, Wh], "*", Wh),     ([Wh, Wh], "*", Wh),   ([Q, Wh], "*", Null),
+                                   ([Wh, Q], "*", Null) ]
+  in
+    (states, syms, f, trans)
+
+fsta_wh2 :: Automaton State String
+fsta_wh2 =
+  let
+    states = [Null, Wh, Q]
+    syms = plainWords ++ whWords ++ qWords ++ ["*","**"]
+    f = [Null]
+    trans = 
+      -- plainWords leaf nodes
+      map (\w -> ([], w, Null)) plainWords ++
+      -- whWords leaf nodes
+      map (\w -> ([], w, Wh)) whWords ++
+      -- qWords leaf nodes
+      map (\w -> ([], w, Q)) qWords ++
+      -- non-terminal nodes
+      -- Q-states not being allowed to have children guarantees Q will c-command wh-words
+      [ ([Null, Null], "*", Null), ([Wh, Null], "*", Wh),
+        ([Null, Wh], "*", Wh),     ([Wh, Wh], "*", Wh),   ([Q, Wh], "*", Null),
+                                   ([Wh, Q], "*", Null) ] ++
+      -- account for adjunct restrictions
+      -- Do not allow non-[Null, Null] or non-[Q, Wh] format transitions with symbol "**"
+      -- Enforces wh-licensing within adjuncts since children of "**" nodes still follow
+      -- rules in transitions above
+      [ ([Null, Null], "**", Null), ([Q, Wh], "**", Null), ([Wh, Q], "**", Null) ]
+  in
+    (states, syms, f, trans)
