69340f73ec5dbc29c6af7da554d21888c202c4a6
arthur.kim@g.ucla.edu <arthur.kim@g.ucla.edu>
2023-10-09 14:42:21 -0700

    code for problem 4

--- f23/cs131/hw1/hw1.hs
+++ f23/cs131/hw1/hw1.hs
@@ -13,3 +13,28 @@ all_factors :: Integer -> [Integer]
 all_factors x = [y | y <- [1..x], mod x y == 0]
 
 perfect_numbers = [x | x <- [1..], (sum (init (all_factors x))) == x]
+
+is_even_if :: Integer -> Bool
+is_even_if x = if (x == 0) then True else
+                 if (x == 1) then False else
+                   is_even_if (x-2)
+
+is_odd_if :: Integer -> Bool
+is_odd_if x = is_even_if (x+1)
+
+is_even_guards :: Integer -> Bool
+is_even_guards x
+  | x == 0 = True
+  | x == 1 = False
+  | otherwise = is_even_guards (x-2)
+
+is_odd_guards :: Integer -> Bool
+is_odd_guards x = is_even_guards (x+1)
+
+is_even_pattern :: Integer -> Bool
+is_even_pattern 0 = True
+is_even_pattern 1 = False
+is_even_pattern x = is_even_pattern (x-2)
+
+is_odd_pattern :: Integer -> Bool
+is_odd_pattern x = is_even_pattern (x+1)
