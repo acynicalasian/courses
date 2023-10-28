b034853949d6bd231592daa6cdb32d50d6372ce5
arthur.kim@g.ucla.edu <arthur.kim@g.ucla.edu>
2023-10-27 17:49:26 -0700

    moved the solution from Nach to p1sol

--- f23/cs131/p2/interpreterv1.py
+++ f23/cs131/p2/interpreterv1.py
@@ -1,130 +1,168 @@
-from env_v1 import EnvironmentManager
-from type_valuev1 import Type, Value, create_value, get_printable
-from intbase import InterpreterBase, ErrorType
 from brewparse import parse_program
+import intbase
 
-
-# Main interpreter class
-class Interpreter(InterpreterBase):
-    # constants
-    NIL_VALUE = create_value(InterpreterBase.NIL_DEF)
-    BIN_OPS = {"+", "-"}
-
-    # methods
+class Interpreter(intbase.InterpreterBase):
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp)
-        self.trace_output = trace_output
-        self.__setup_ops()
+        self._vtable = dict()
 
-    # run a program that's provided in a string
-    # usese the provided Parser found in brewparse.py to parse the program
-    # into an abstract syntax tree (ast)
     def run(self, program):
-        ast = parse_program(program)
-        self.__set_up_function_table(ast)
-        main_func = self.__get_func_by_name("main")
-        self.env = EnvironmentManager()
-        self.__run_statements(main_func.get("statements"))
+        parsed_program = parse_program(program)
+        if len(parsed_program.dict["functions"]) > 1:
+            # spec says program node will only have one child function node
+            super().error(intbase.ErrorType.NAME_ERROR,
+                          "Program must only have function main() defined.")
+        main_node = parsed_program.dict["functions"][0]
+        if main_node.dict["name"] != "main":
+            super().error(intbase.ErrorType.NAME_ERROR,
+                          "No main() function was found")
+        for s in main_node.dict["statements"]:
+            if s.elem_type == '=':
+                self.eval_assign(s)
+            elif s.elem_type == "fcall":
+                self.eval_fcall(s)
+            else:
+                super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
+                
 
-    def __set_up_function_table(self, ast):
-        self.func_name_to_ast = {}
-        for func_def in ast.get("functions"):
-            self.func_name_to_ast[func_def.get("name")] = func_def
+    def eval_assign(self, s):
+        lh = s.dict["name"]
+        rh_node = s.dict["expression"]
+        rh = None
+        if rh_node.elem_type in ['+', '-']:
+            rh = self.eval_binop(rh_node, rh_node.elem_type)
+        elif rh_node.elem_type == "var":
+            try:
+                vname = rh_node.dict["name"]
+                rh = self._vtable[vname]
+            except KeyError:
+                super().error(intbase.ErrorType.NAME_ERROR,
+                              "Variable '" + vname + "' is not defined")
+        elif rh_node.elem_type in ["int", "string"]:
+            rh = rh_node.dict["val"]
+        elif rh_node.elem_type == "fcall":
+            rh = self.eval_fcall(rh_node)
+        else:
+            super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
+        self._vtable[lh] = rh
+        
+    def eval_binop(self, exp, op):
+        if exp.dict["op1"].elem_type in ['+', '-']:
+            op1 = self.eval_binop(exp.dict["op1"], exp.dict["op1"].elem_type)
+        elif exp.dict["op1"].elem_type == "var":
+            try:
+                vname = exp.dict["op1"].dict["name"]
+                op1 = self._vtable[vname]
+            except KeyError:
+                super().error(intbase.ErrorType.NAME_ERROR,
+                              "Variable '" + vname + "' is not defined")
+        elif exp.dict["op1"].elem_type in ["int", "string"]:
+            op1 = exp.dict["op1"].dict["val"]
+        elif (exp.dict["op1"].elem_type == "fcall" and
+              exp.dict["op1"].dict["name"] == "inputi"):
+            op1 = self.eval_fcall(exp.dict["op1"])
+        elif (exp.dict["op1"].elem_type == "fcall" and
+              exp.dict["op1"].dict["name"] == "print"):
+            super().error(intbase.ErrorType.TYPE_ERROR,
+                          "Operand op1 is of invalid type void")
+        else:
+            super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
 
-    def __get_func_by_name(self, name):
-        if name not in self.func_name_to_ast:
-            super().error(ErrorType.NAME_ERROR, f"Function {name} not found")
-        return self.func_name_to_ast[name]
+        if exp.dict["op2"].elem_type in ['+', '-']:
+            op2 = self.eval_binop(exp.dict["op2"], exp.dict["op2"].elem_type)
+        elif exp.dict["op2"].elem_type == "var":
+            try:
+                vname = exp.dict["op2"].dict["name"]
+                op2 = self._vtable[vname]
+            except KeyError:
+                super().error(intbase.ErrorType.NAME_ERROR,
+                              "Variable '" + vname + "' is not defined")
+        elif exp.dict["op2"].elem_type in ["int", "string"]:
+            op2 = exp.dict["op2"].dict["val"]
+        elif (exp.dict["op2"].elem_type == "fcall" and
+              exp.dict["op2"].dict["name"] == "inputi"):
+            op2 = self.eval_fcall(exp.dict["op2"])
+        elif (exp.dict["op2"].elem_type == "fcall" and
+              exp.dict["op2"].dict["name"] == "print"):
+            super().error(intbase.ErrorType.TYPE_ERROR,
+                          "Operand op2 is of invalid type void")
+        else:
+            super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
 
-    def __run_statements(self, statements):
-        # all statements of a function are held in arg3 of the function AST node
-        for statement in statements:
-            if self.trace_output:
-                print(statement)
-            if statement.elem_type == InterpreterBase.FCALL_DEF:
-                self.__call_func(statement)
-            elif statement.elem_type == "=":
-                self.__assign(statement)
+        if isinstance(op1, str) or isinstance(op2, str):
+            super().error(intbase.ErrorType.TYPE_ERROR,
+                          "Incompatible types for arithmetic operation")
+        elif op == '-':
+            return op1 - op2
+        else:
+            return op1 + op2
 
-        return Interpreter.NIL_VALUE
+    def eval_fcall(self, f):
+        if f.dict["name"] == "print":
+            if len(f.dict["args"]) == 0:
+                super().output("")
+                return "nil"
+            acc = ""
+            for arg in f.dict["args"]:
+                if arg.elem_type in ['+', '-']:
+                    acc += str(self.eval_binop(arg, arg.elem_type))
+                elif arg.elem_type == "var":
+                    try:
+                        vname = arg.dict["name"]
+                        tmpv = self._vtable[vname]
+                    except KeyError:
+                        super().error(intbase.ErrorType.NAME_ERROR,
+                                      "Variable '" + vname + "' is not defined")
+                    acc += str(tmpv)
+                elif arg.elem_type in ["int", "string"]:
+                    acc += str(arg.dict["val"])
+                elif arg.elem_type == "fcall" and arg.dict["name"] == "print":
+                    super().error(intbase.ErrorType.TYPE_ERROR,
+                                  """Argument to function 'print' cannot be of
+                                  type void""")
+                elif arg.elem_type == "fcall" and arg.dict["name"] == "inputi":
+                    # I seriously doubt this would happen but...
+                    acc += str(self.eval_fcall(arg))
+                else:
+                    super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
+            super().output(acc)
+            return "nil"
+        elif f.dict["name"] == "inputi":
+            if len(f.dict["args"]) == 1:
+                prompt = f.dict["args"][0]
+                if prompt.elem_type in ['+', '-']:
+                    super().output(
+                        str(self.eval_binop(prompt, prompt.elem_type)))
+                elif prompt.elem_type == "var":
+                    try:
+                        vname = prompt.dict["name"]
+                        tmpv = self._vtable[vname]
+                    except KeyError:
+                        super().error(intbase.ErrorType.NAME_ERROR,
+                                      "Variable '" + vname + "' is not defined")
+                    super().output(str(tmpv))
+                elif prompt.elem_type in ["int", "string"]:
+                    super().output(str(prompt.dict["val"]))
+                elif prompt.elem_type == "fcall" and prompt.dict["name"] == "print":
+                    # super().error(intbase.ErrorType.TYPE_ERROR,
+                    #               """Argument to function 'inputi' cannot be of
+                    #               type void""")
 
-    def __call_func(self, call_node):
-        func_name = call_node.get("name")
-        if func_name == "print":
-            return self.__call_print(call_node)
-        if func_name == "inputi":
-            return self.__call_input(call_node)
-
-        # add code here later to call other functions
-        super().error(ErrorType.NAME_ERROR, f"Function {func_name} not found")
-
-    def __call_print(self, call_ast):
-        output = ""
-        for arg in call_ast.get("args"):
-            result = self.__eval_expr(arg)  # result is a Value object
-            output = output + get_printable(result)
-        super().output(output)
-        return Interpreter.NIL_VALUE
-
-    def __call_input(self, call_ast):
-        args = call_ast.get("args")
-        if args is not None and len(args) == 1:
-            result = self.__eval_expr(args[0])
-            super().output(get_printable(result))
-        elif args is not None and len(args) > 1:
-            super().error(
-                ErrorType.NAME_ERROR, "No inputi() function that takes > 1 parameter"
-            )
-        inp = super().get_input()
-        if call_ast.get("name") == "inputi":
-            return Value(Type.INT, int(inp))
-        # we can support inputs here later
-
-    def __assign(self, assign_ast):
-        var_name = assign_ast.get("name")
-        value_obj = self.__eval_expr(assign_ast.get("expression"))
-        self.env.set(var_name, value_obj)
-
-    def __eval_expr(self, expr_ast):
-        if expr_ast.elem_type == InterpreterBase.INT_DEF:
-            return Value(Type.INT, expr_ast.get("val"))
-        if expr_ast.elem_type == InterpreterBase.STRING_DEF:
-            return Value(Type.STRING, expr_ast.get("val"))
-        if expr_ast.elem_type == InterpreterBase.VAR_DEF:
-            var_name = expr_ast.get("name")
-            val = self.env.get(var_name)
-            if val is None:
-                super().error(ErrorType.NAME_ERROR, f"Variable {var_name} not found")
-            return val
-        if expr_ast.elem_type == InterpreterBase.FCALL_DEF:
-            return self.__call_func(expr_ast)
-        if expr_ast.elem_type in Interpreter.BIN_OPS:
-            return self.__eval_op(expr_ast)
-
-    def __eval_op(self, arith_ast):
-        left_value_obj = self.__eval_expr(arith_ast.get("op1"))
-        right_value_obj = self.__eval_expr(arith_ast.get("op2"))
-        if left_value_obj.type() != right_value_obj.type():
-            super().error(
-                ErrorType.TYPE_ERROR,
-                f"Incompatible types for {arith_ast.elem_type} operation",
-            )
-        if arith_ast.elem_type not in self.op_to_lambda[left_value_obj.type()]:
-            super().error(
-                ErrorType.TYPE_ERROR,
-                f"Incompatible operator {arith_ast.get_type} for type {left_value_obj.type()}",
-            )
-        f = self.op_to_lambda[left_value_obj.type()][arith_ast.elem_type]
-        return f(left_value_obj, right_value_obj)
-
-    def __setup_ops(self):
-        self.op_to_lambda = {}
-        # set up operations on integers
-        self.op_to_lambda[Type.INT] = {}
-        self.op_to_lambda[Type.INT]["+"] = lambda x, y: Value(
-            x.type(), x.value() + y.value()
-        )
-        self.op_to_lambda[Type.INT]["-"] = lambda x, y: Value(
-            x.type(), x.value() - y.value()
-        )
-        # add other operators here later for int, string, bool, etc
+                    # Apparently we can pass print() as an argument to inputi(); is
+                    # this the testcase3 I keep missing?
+                    super().output(self.eval_fcall(prompt))
+                elif prompt.elem_type == "fcall" and prompt.dict["name"] == "inputi":
+                    super().output(self.eval_fcall(prompt))
+                elif prompt.elem_type == "fcall" and prompt.dict["name"] == "main":
+                    # Is this the test case 3 I'm missing for input?
+                    super().error(intbase.ErrorType.TYPE_ERROR,
+                                  """Cannot call main() from within main()""")
+                else:
+                    super().error(intbase.ErrorType.NAME_ERROR, "this shouldn't happen")
+            elif len(f.dict["args"]) > 1:
+                super().error(intbase.ErrorType.NAME_ERROR,
+                              "No inputi() function that takes > 1 parameter")
+            return int(super().get_input())
+        else:
+            super().error(intbase.ErrorType.NAME_ERROR,
+                          "Function '" + f.dict["name"] + "' is not defined")
