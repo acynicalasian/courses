592c52fe4c3b0abaa0544cff39341a6d3ee98f33
Arthur Kim <arthur.kim@g.ucla.edu>
2023-12-01 21:35:31 -0800

    worked on p4

---
+++ f23/cs131/p4/type_valuev4.py
@@ -0,0 +1,77 @@
+import copy
+
+from enum import Enum
+from intbase import InterpreterBase
+
+
+# Enumerated type for our different language data types
+class Type(Enum):
+    INT = 1
+    BOOL = 2
+    STRING = 3
+    CLOSURE = 4
+    NIL = 5
+    # My addition on top of type_valuev3: object enum
+    OBJECT = 6
+
+
+class Closure:
+    def __init__(self, func_ast, env):
+        self.captured_env = copy.deepcopy(env)
+        # Edit to Carey's v3: Objects and closures are captured by reference
+        for e, e_cp in zip(env.environment, self.captured_env.environment):
+            for vname in e:
+                if isinstance(e[vname].v, Object) or isinstance(e[vname].v, Closure):
+                    e_cp[vname] = e[vname]                                            
+        self.func_ast = func_ast
+        self.type = Type.CLOSURE
+
+
+# Represents a value, which has a type and its value
+class Value:
+    def __init__(self, t, v=None):
+        self.t = t
+        self.v = v
+
+    def value(self):
+        return self.v
+
+    def type(self):
+        return self.t
+
+    def set(self, other):
+        self.t = other.t
+        self.v = other.v
+
+# My addition on top of type_valuev3: object type
+class Object:
+    def __init__(self):
+        # Treat vars and methods identically as first-class for now: keep them all in self.fields
+        self.fields = dict()
+        self.proto = None
+        
+        
+
+def create_value(val):
+    if val == InterpreterBase.TRUE_DEF:
+        return Value(Type.BOOL, True)
+    elif val == InterpreterBase.FALSE_DEF:
+        return Value(Type.BOOL, False)
+    elif isinstance(val, int):
+        return Value(Type.INT, val)
+    elif val == InterpreterBase.NIL_DEF:
+        return Value(Type.NIL, None)
+    elif isinstance(val, str):
+        return Value(Type.STRING, val)
+
+
+def get_printable(val):
+    if val.type() == Type.INT:
+        return str(val.value())
+    if val.type() == Type.STRING:
+        return val.value()
+    if val.type() == Type.BOOL:
+        if val.value() is True:
+            return "true"
+        return "false"
+    return None
