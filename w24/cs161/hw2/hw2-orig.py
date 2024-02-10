3b41e094cd7cdfb15758e9c224b87f7ca96b2ed5
Arthur Kim <arthur.kim@g.ucla.edu>
2024-02-09 23:35:52 -0800

    decided on using h_manhattan as my solution, but h0 does seem faster a lot of the time...

--- w24/cs161/hw2/hw2-orig.py
+++ w24/cs161/hw2/hw2-orig.py
@@ -131,16 +131,7 @@ def cleanUpList(s_list):
 # this function as the goal testing function, A* will never
 # terminate until the whole search space is exhausted.
 def goal_test(s):
-    # If we have a box, we either haven't solved/failed yet, or the problem isn't
-    # solvable because we have more boxes than goals
-    row = s.shape[0]
-    col = s.shape[1]
-    for i in range(row):
-        for j in range(col):
-            if isBox(s[i,j]):
-                return False
-    return True
-
+    raise NotImplementedError()
 
 
 # EXERCISE: Modify this function to return the list of
@@ -163,110 +154,23 @@ def goal_test(s):
 def next_states(s):
     row, col = getKeeperPosition(s)
     s_list = []
-
-    ### Edited: commented this out for now to only create array copies if the conditions are right
-    #s1 = np.copy(s)
+    s1 = np.copy(s)
 
     # NOT IMPLEMENTED YET! YOU NEED TO FINISH THIS FUNCTION.
 
-    ### Beginning of my implementation:
-    height, width = np.shape(s)
-
-    # check UP DOWN LEFT RIGHT
-    adjacent_coords = [(row-1,col), (row+1,col), (row,col-1), (row,col+1)]
-    push_coords = [(row-2,col), (row+2,col), (row,col-2), (row,col+2)]
-    for i in range(4):
-        (adj_y, adj_x) = adjacent_coords[i]
-        # Skip if adj_y or adj_x coordinates are invalid
-        if adj_y < 0 or adj_y >= height or adj_x < 0 or adj_x >= width:
-            continue
-        # Walkable? So direction we walk is either blank or goal
-        if isBlank(s[adj_y, adj_x]) or isStar(s[adj_y, adj_x]):
-            s_copy = np.copy(s)
-            # Are we stepping off a goal or a blank?
-            if isKeeper(s_copy[row, col]):
-                s_copy[row, col] = blank
-            elif isKeeperstar(s_copy[row, col]):
-                s_copy[row, col] = star
-            else: # this shouldn't happen, we have a logic error
-                assert False, "logic error"
-            # Are we stepping onto a blank or a goal?
-            if isBlank(s_copy[adj_y, adj_x]):
-                s_copy[adj_y, adj_x] = keeper
-            elif isStar(s_copy[adj_y, adj_x]):
-                s_copy[adj_y, adj_x] = keeperstar
-            else:
-                assert False, "logic error"
-            s_list.append(s_copy)
-        # We walk into a wall
-        elif isWall(s[adj_y, adj_x]):
-            continue
-        # We walk into a box
-        elif isBox(s[adj_y, adj_x]) or isBoxstar(s[adj_y, adj_x]):
-            # Are we pushing onto a blank, goal, or box/wall?
-            (push_y, push_x) = push_coords[i]
-            # Skip if push coordinates are invalid
-            if push_y < 0 or push_y >= height or push_x < 0 or push_x >= width:
-                continue
-            if isBlank(s[push_y, push_x]) or isStar(s[push_y, push_x]):
-                # Did we push a box into a blank space where a corner is? If so, we have no way to solve
-                # so don't generate new states
-                if isBlank(s[push_y, push_x]) and push_y > 0 and isWall(s[push_y - 1, push_x]): # check up
-                    if push_x > 0 and isWall(s[push_y, push_x - 1]): # check left
-                        continue
-                    if push_x < width - 1 and isWall(s[push_y, push_x + 1]): # check right
-                        continue
-                if isBlank(s[push_y, push_x]) and push_y < height - 1 and isWall(s[push_y + 1, push_x]): # check down
-                    if push_x > 0 and isWall(s[push_y, push_x - 1]): # check left
-                        continue
-                    if push_x < width - 1 and isWall(s[push_y, push_x + 1]): # check right
-                        continue
-                # Only bother to copy if we're pushing into a pushable area
-                s_copy = np.copy(s)
-                # Are we stepping off a blank or a goal?
-                s_copy[row, col] = blank if isKeeper(s[row, col]) else star
-                # Are we pushing a box off and stepping onto a blank or a goal?
-                s_copy[adj_y, adj_x] = keeper if isBox(s[adj_y, adj_x]) else keeperstar
-                # Are we pushing a box onto a blank or a goal?
-                s_copy[push_y, push_x] = box if isBlank(s[push_y, push_x]) else boxstar
-                s_list.append(s_copy)
-            elif isWall(s[push_y, push_x]) or isBox(s[push_y, push_x]) or isBoxstar(s[push_y, push_x]):
-                continue
-            else:
-                assert False, "logic error"
-        else:
-            assert False, "we walked into another keeper? tf"
-    
-    return s_list
-    ### End of my implementation
-
-    ### Edited: commented this out for now since I *think* my implementation doesn't add Nones
-    # return cleanUpList(s_list)
+    return cleanUpList(s_list)
 
 
 # EXERCISE: Modify this function to compute the trivial
 # admissible heuristic.
 def h0(s):
-    return 0
+    raise NotImplementedError()
 
 
 # EXERCISE: Modify this function to compute the
 # number of misplaced boxes in state s (numpy array).
-#
-# My answer to HW2 problem:
-# -----------------------------
-#
-# This heuristic is admissible because the lowest possible number of steps to a game with n misplaced boxes is n steps
 def h1(s):
-    count = 0
-    (row, col) = np.shape(s)
-    for i in range(row):
-        for j in range(col):
-            if isBox(s[i,j]):
-                count += 1
-    return count
-    
-    
+    raise NotImplementedError()
 
 
 # EXERCISE: Change the name of this function to h<UID> where
@@ -276,157 +180,9 @@ def h1(s):
 # This function will be tested in various hard examples.
 # Objective: make A* solve problems as fast as possible.
 
-def h004919548(s):
-    #return h_greedy(s)
-    return h_manhattan(s)
+def hUID(s):
+    raise NotImplementedError()
 
-def h_greedy(s):
-    """
-    We use a greedy approach for this heuristic. Choose a box and corresponding goal that gives the lowest cost 
-    for moving just that box. For example, if there's one box that's 1m away, and the closest goal to that box is 
-    4m away, and there's a second box that's 2m away but the closest goal to that box is 2m away from it, moving 
-    the second box to the goal that's 2m away from it would be the greedy choice.
-
-    This heuristic function is admissible for a number of reasons. For n = 1 box, this approach solves the 
-    problem trivially, but for greater than that, note the following:
-
-    If the greedy choice is optimal, then the total cost of moving every box is always higher than the cost of 
-    moving the first box greedily, especially since you cannot move more than one box at a time. If the greedy 
-    choice would not be optimal, that implies that the moving the first box wherever has a higher cost than 
-    moving the box we chose with the greedy method.
-
-    Finally, this heuristic function relies on straight-line distances, and due to the geometry/constraints of 
-    the problem, straight-line distances are always at least the distance/steps needed in Sokoban.
-
-    Every time we calculate this heuristic, we search the entire field for misplaced boxes in the current state.
-    Assume that's size s^2. Then, for n misplaced boxes and >= n goals in a solvable puzzle, we calculate the 
-    Euclidean distance to the nearest box, and then we search for the nearest goal. This gives n^2. So the
-    overall time complexity is roughly O(s^2).
-    """
-    (keeper_y, keeper_x) = getKeeperPosition(s)
-    (height, width) = np.shape(s)
-    box_coords = []
-    goal_coords = []
-    for i in range(height):
-        for j in range(width):
-            if isBox(s[i,j]):
-                box_coords.append((i,j))
-            if isStar(s[i,j]):
-                goal_coords.append((i,j))
-    minval = None
-    # No more boxes? Return 0
-    # Not sure why we need this second condition but was failing otherwise
-    # But I did have a bug in state generator which may be why I needed it
-    if not box_coords or not goal_coords:
-        return 0
-    for (box_y, box_x) in box_coords:
-        # Find the nearest box
-        a2_box = (box_y - keeper_y) * (box_y - keeper_y)
-        b2_box = (box_x - keeper_x) * (box_x - keeper_x)
-        c_box = np.sqrt(a2_box + b2_box)
-        for (goal_y, goal_x) in goal_coords:
-            # Find the nearest goal to the nearest box
-            a2_goal = (goal_y - box_y) * (goal_y - box_y)
-            b2_goal = (goal_x - box_x) * (goal_x - box_x)
-            c_goal = np.sqrt(a2_goal + b2_goal)
-            hestimate = c_box + c_goal
-            if minval is None:
-                minval = hestimate
-            elif hestimate < minval:
-                minval = hestimate
-    assert minval is not None, "Logic error in heuristic fn"
-    return int(minval)
-
-def h_manhattan(s):
-    """
-    Same principles as before. Let's try measuring both a box distance and the goal distance with the 
-    manhattan distance. O(s^2) complexity.
-    """
-    (keeper_y, keeper_x) = getKeeperPosition(s)
-    (height, width) = np.shape(s)
-    box_coords = []
-    goal_coords = []
-    for i in range(height):
-        for j in range(width):
-            if isBox(s[i,j]):
-                box_coords.append((i,j))
-            if isStar(s[i,j]):
-                goal_coords.append((i,j))
-    minval = None
-    if not box_coords or not goal_coords:
-        return 0
-    for (box_y, box_x) in box_coords:
-        c_box = abs(box_y - keeper_y) + abs(box_x - keeper_x)
-        for (goal_y, goal_x) in goal_coords:
-            c_goal = abs(goal_y - box_y) + abs(goal_x - box_x)
-            hestimate = c_box + c_goal
-            if minval is None:
-                minval = hestimate
-            elif hestimate < minval:
-                minval = hestimate
-    assert minval is not None, "Logic error in manhattan fn"
-    return int(minval)
-
-def solve_from_cli(puzzle):
-    currwidth = 0
-    maxwidth = 0
-    parsed_puzzle = []
-    lineaccum = []
-    for i in range(len(puzzle)):
-        if not puzzle[i] == '\n':
-            currwidth += 1
-        if puzzle[i] == '#':
-            lineaccum.append(wall)
-        elif puzzle[i] == '@':
-            lineaccum.append(keeper)
-        elif puzzle[i] == '+':
-            lineaccum.append(keeperstar)
-        elif puzzle[i] == '$':
-            lineaccum.append(box)
-        elif puzzle[i] == '*':
-            lineaccum.append(boxstar)
-        elif puzzle[i] == '.':
-            lineaccum.append(star)
-        elif puzzle[i] == ' ':
-            lineaccum.append(blank)
-        elif puzzle[i] == '\n':
-            if currwidth > maxwidth:
-                maxwidth = currwidth
-            currwidth = 0
-            parsed_puzzle.append(lineaccum)
-            lineaccum = []
-        else:
-            assert False, "unexpected input!"
-    if lineaccum:
-        parsed_puzzle.append(lineaccum)
-    # Pad lines so they line up
-    padded_puzzle = []
-    for line in parsed_puzzle:
-        if len(line) < maxwidth:
-            new_line = line + [blank for i in range(maxwidth - len(line))]
-            padded_puzzle.append(new_line)
-        else:
-            padded_puzzle.append(line)
-    # Sanity check
-    for line in padded_puzzle:
-        assert len(line) == maxwidth, "logic error!"
-        assert isinstance(line, list), "logic error!"
-        assert isinstance(line[0], int)
-        
-    import time
-    start_time = time.time()
-    sokoban(padded_puzzle, h0)
-    print("h0: ", str(time.time() - start_time), '\n')
-    start_time = time.time()
-    sokoban(padded_puzzle, h1)
-    print("h1: ", str(time.time() - start_time), '\n')
-    start_time = time.time()
-    sokoban(padded_puzzle, h_manhattan)
-    print("h_manhattan: ", str(time.time() - start_time), '\n')
-    
-            
-        
-            
 
 # Some predefined problems with initial state s (array). Sokoban function will automatically transform it to numpy
 # array. For other function, the state s is presented as a numpy array. You can just call sokoban(init-state,
@@ -702,26 +458,3 @@ if __name__ == "__main__":
     sokoban(s3, h0)
 
     sokoban(s4, h0)
-
-    
-    # import time
-    # start_time = time.time()
-    # sokoban(s19, h0)
-    # print("h0: ", str(time.time() - start_time))
-    # start_time = time.time()
-    # sokoban(s19, h1)
-    # print("h1: ", str(time.time() - start_time))
-    # start_time = time.time()
-    # sokoban(s19, h_greedy)
-    # print("h_greedy: ", str(time.time() - start_time))
-    # start_time = time.time()
-    # sokoban(s19, h_boxonly_greedy)
-    # print("h_boxonly_greedy: ", str(time.time() - start_time))
-    # start_time = time.time()
-    # sokoban(s19, h_manhattan)
-    # print("h_manhattan: ", str(time.time() - start_time))
-    # start_time = time.time()
-    # sokoban(s19, h_boxonly_manhattan)
-    # print("h_boxonly_manhattan: ", str(time.time() - start_time))
-    
-    
